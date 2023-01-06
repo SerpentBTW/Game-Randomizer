344050f85baeb8266fb25efe6f42be6df428f535
SerpentBTW <serpbtw@gmail.com>
2023-01-06 18:13:55 -0500

    Test

---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+import random
+
+with open("gameRandomizer\main.json") as f:
+  data = json.load(f)
+
+
+gameCount = len(data["Games"])
+rand = random.randint(0, gameCount - 1 )
+print(data["Games"][rand])
